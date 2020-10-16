32d88b27171ab094180d40da26b923851736a02c
twl-itxiaobai <twl19981125@163.com>
2020-10-16 10:26:31 +0800

    twl

--- RTMPLIVE-client/src/pages/admin/child_pages/im/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Im extends Component {
-  render() {
-    return <div>聊天室</div>;
-  }
-}
--- RTMPLIVE-client/src/utils/memoryUtils.js
+++
@@ -1,4 +0,0 @@
-import storageUtils from "./storageUtils";
-export default {
-  user: storageUtils.getUser(), //用来存储用户登录的信息，初始值为local中读取的user
-};
